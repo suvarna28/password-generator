24cab7661d4baedc0c013b4b85ecd80b446ab7f4
Suvarna Jadhav <jadhav.suvarna28@gmail.com>
2022-11-26 15:56:34 -0500

    added first draft of the application code

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # password-generator
+
+## Description 
+
+This is an application that generators ramdom passwords based on user's requirement. The started code containing HTML and CSS is provided, the challenge here was to build the logic. Give it try !!
+
+* Click on the "Genarate Password" button to start.
+* Then it asks for password length, we have considered a password length range of 8 or greater and 128 or smaller. If you select a length other than this then it gives an alert box asking to select the specified range. 
+* Then it asks for a series of questions to select special characters, numbers, small letters and capital letters. 
+* If not a single character is selected then it gives an alert message and then the program starts over.
+* At last, it generates the password based on user requirements.
+
+## Features
+
+* It is responsive, this feature is implemented in the starter code. 
+* Rest of the features are same as explained in the description, please refer above.
+
+## Installation
+
+N/A
+
+## Screenshot
+
+![Main Webpage](./screenshot/screenshot.png)
+
+## Link to the deployed application
+
+https://suvarna28.github.io/password-generator/
+
+## Credits
+
+* Myles Smith, for discussing his logic and approach in the class while discussing the challenge. The logic is based on his approach
+* Our Instructor Gabrial Perry, for discussing Myles's approach
+
---
+++ script.js
@@ -0,0 +1,92 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+
+  /* Below is the declaration and definition of the different variables and arrays used in this function */
+
+  var selectedPwdLength;
+
+  var finalPwdArray = []; 
+
+  var finalPwd = "";
+  
+  var specialChars = ['~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']',
+                      '|', '\\', ':', ';', '\"', '\'', '<', '>', ',', '.', ' ', '?', '/'];
+
+  var capitalLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+                          'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+  var smallLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+                          't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+  var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+  /* Code to enter the password length and to check if it falls within the required range */
+
+  var pwdLength = prompt("Please enter the length of password you wish to create");
+
+  if(parseInt(pwdLength) >= 8 && parseInt(pwdLength) <= 128){
+    selectedPwdLength = parseInt(pwdLength) 
+  } 
+  else {
+    alert("Password should be atleast 8 characters long and less than 128 characters");
+    generatePassword();
+  }
+
+/* Code to ask the different criteria for the password strenth, if it should contain capital letters, small letters, numbers and special characters */
+
+  var specialCharsY = confirm("Do you want any special characters in your password")
+  var numbersY = confirm("Do you want any numbers in your password")
+  var capitalLettersY = confirm("Do you want any capital letters in your password")
+  var smallLettersY = confirm("Do you want any small letters in your password")
+
+/* Code to verify if at least one criteria is selected, if not then start over */
+
+  if(!specialCharsY && !numbersY && !capitalLettersY && !smallLettersY){
+    alert("Please select atleast one criteria to create a password.");
+    generatePassword();
+  }
+
+/* Code to check which criterias were selected and then to build a final array containing all those values */
+
+  if(specialCharsY) {
+    finalPwdArray = finalPwdArray.concat(specialChars);
+  }
+  if(numbersY) {
+    finalPwdArray = finalPwdArray.concat(numbers);
+  }
+  if(capitalLettersY) {
+    finalPwdArray = finalPwdArray.concat(capitalLetters);
+  }
+  if(smallLettersY) {
+    finalPwdArray = finalPwdArray.concat(smallLetters);
+  }
+
+/* Code to actually build the final password, 
+  the number of times 'for' loop is executed is equal to the password length selected by the user, 
+  because that's how many times the loop will execute and Math.random() will select
+  a random index and it's respective character from the final array. Build the actual pwd at the end by concatenating the characters */
+
+  for(var i = 0; i < selectedPwdLength; i++){
+    var index = Math.floor(Math.random() * finalPwdArray.length);
+    finalPwd = finalPwd + finalPwdArray[index];
+  }
+
+  return finalPwd;
+
+}
+ 
+
+
